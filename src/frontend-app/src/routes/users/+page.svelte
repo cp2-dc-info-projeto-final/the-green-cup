4ec4dd3a5225a3b6c3e78bef882ce75aee3bc255
cstmateuzx <jennings1808@gmail.com>
2025-08-16 13:27:22 -0300

    Sprint 4

--- src/frontend-app/src/routes/users/+page.svelte
+++ src/frontend-app/src/routes/users/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { A, Heading } from 'flowbite-svelte';
+  import { Heading } from 'flowbite-svelte';
   import UsersTable from '../../components/UsersTable.svelte';
   import { UserAddOutline  } from 'flowbite-svelte-icons';
   import { goto } from '$app/navigation';
@@ -15,7 +15,7 @@
 
   usuarios.pop()
   let erro = "";
-  let search = "";
+  let pesquisa = "";
 
   //Consulta usuario por filtro
   async function consultaUsuario() {
@@ -34,9 +34,14 @@
     }
   }
   //Consulta por nome
-  async function consultaNome(login) {
+  async function filtro() {
+
+      usuarios.filter(usuarios =>
+      usuarios.login.toLowerCase().includes(pesquisa.toLowerCase())
+    );
+    
     try{
-      const res = await fetch("http://localhost:3000/users/login/${login}");
+      const res = await fetch("http://localhost:3000/users");
       const data = await res.json();
 
       if (!data.sucess) {
@@ -56,18 +61,33 @@
   onMount(() => {
       consultaUsuario();
   });
+/*
 
+  let pesquisa = '';
+  let usuarios = [{
+      id: 0,
+      login: "",
+      email: ""
+  }];
+  let filtros = usuarios;
+
+  function filtro() {
+    filtros = usuarios.filter(usuarios =>
+      usuarios.login.toLowerCase().includes(pesquisa.toLowerCase())
+    );
+  }
+*/
 </script>
 
 <div class="text-center p-8 pt-32">
   <div class="flex items-center justify-between max-w-3xl mx-auto mb-6">
     <Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Usuários</Heading>
     <div>
-      <Search placeholder="Pesquisar" bind:value={search}>
-        <Button class="me-1" on:click={() => consultaNome('/users/login')}>Pesquisar</Button>
+      <Search placeholder="Pesquisar" bind:value={pesquisa}>
+        <Button class="me-1" onclick={filtro}>Pesquisar</Button>
       </Search>
-    </div>
-    <button class="flex items-center gap-2 px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg font-semibold shadow transition" on:click={() => goto('/users/new')}>
+      </div>
+    <button class="flex items-center gap-2 px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg font-semibold shadow transition" onclick={() => goto('/users/new')}>
       <UserAddOutline class="w-5 h-5" />
       Adicionar
     </button>
