9c2bceec79c5eb9799508a0abf84edf3890a8a0a
root <root@DESKTOP-RKJL9IK.localdomain>
2025-08-16 23:57:04 -0300

    Ajuste fino

--- src/frontend-app/src/routes/users/+page.svelte
+++ src/frontend-app/src/routes/users/+page.svelte
@@ -4,7 +4,6 @@
   import { UserAddOutline  } from 'flowbite-svelte-icons';
   import { goto } from '$app/navigation';
   import { Search, Button } from "flowbite-svelte";
-  import { onMount } from 'svelte';
 
 
   let usuarios = [{
@@ -17,31 +16,15 @@
   let erro = "";
   let pesquisa = "";
 
-  //Consulta usuario por filtro
-  async function consultaUsuario() {
-    try{
-      const res = await fetch('http://localhost:3000/users/');
-      const data = await res.json();
-
-      if (!data.sucess) {
-        erro = data.message || 'Nenhum usuário encontrado';
-        return;
-      }
-
-      usuarios = data.data;
-    } catch (e) {
-      erro = 'Servidor não conectado';
-    }
-  }
   //Consulta por nome
-  async function filtro() {
+  async function filtro(pesquisa: string) {
 
       usuarios.filter(usuarios =>
       usuarios.login.toLowerCase().includes(pesquisa.toLowerCase())
     );
     
     try{
-      const res = await fetch("http://localhost:3000/users");
+      const res = await fetch(`http://localhost:3000/users/login/${pesquisa}`);
       const data = await res.json();
 
       if (!data.sucess) {
@@ -58,25 +41,6 @@
     }
   }
 
-  onMount(() => {
-      consultaUsuario();
-  });
-/*
-
-  let pesquisa = '';
-  let usuarios = [{
-      id: 0,
-      login: "",
-      email: ""
-  }];
-  let filtros = usuarios;
-
-  function filtro() {
-    filtros = usuarios.filter(usuarios =>
-      usuarios.login.toLowerCase().includes(pesquisa.toLowerCase())
-    );
-  }
-*/
 </script>
 
 <div class="text-center p-8 pt-32">
@@ -84,7 +48,7 @@
     <Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Usuários</Heading>
     <div>
       <Search placeholder="Pesquisar" bind:value={pesquisa}>
-        <Button class="me-1" onclick={filtro}>Pesquisar</Button>
+        <Button class="me-1" onclick={() => filtro(pesquisa)}>Pesquisar</Button>
       </Search>
       </div>
     <button class="flex items-center gap-2 px-4 py-2 bg-primary-600 hover:bg-primary-700 text-white rounded-lg font-semibold shadow transition" onclick={() => goto('/users/new')}>
