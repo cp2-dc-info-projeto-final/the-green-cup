d419b96913e72db10b6fa4b06c9645d89ae5e6cd
labcaxias <labcaxias@LDC9C6B000EFEB8.labdc.zentyal>
2025-12-04 15:26:30 -0300

    Imagens ajustadas

--- src/frontend-app/src/routes/+page.svelte
+++ src/frontend-app/src/routes/+page.svelte
@@ -23,21 +23,21 @@
                 <p class="mb-3 text-gray-500 dark:text-gray-400">A organização Greenpeace se dedica a proteger a biodiversidade, enfrentar a crise climática, promover soluções sustentáveis e a paz, com financiamento proveniente unicamente de doações de indivíduos. Em 1971, um grupo composto por ecologistas, jornalistas e hippies se reuniu para protestar contra os testes nucleares americanos na costa do Alasca. A resiliência e o comprometimento desses 12 indivíduos, o espírito ativista e uma pequena embarcação de pesca alugada, constituíram os elementos ideais para a criação do Greenpeace. </p>
                 <div class="row-span-1 pb-8">
                     <a href="https://www.greenpeace.org/brasil/">
-                        <img src="https://www.greenpeace.org/static/planet4-brasil-stateless/2022/04/96cc2126-cristo-redentor-30-anos-greenpeace-brasil-1024x683.jpg" alt=""> 
+                        <img src="Greenpeace.jpg" alt=""> 
                     </a>   
                 </div>
                 <p ><a class="text-4xl font-semibold tracking-tight text-green-900 hover:text-green-700 dark:text-white" href="https://institutoterra.org/">INSTITUTO TERRA</a></p>
                 <p class="mb-3 text-gray-500 dark:text-gray-400">O Instituto Terra é uma organização sem fins lucrativos fundada pelos fotógrafos Lélia Wanick Salgado e Sebastião Salgado em 1998, em Aimorés (MG). Sua missão é promover o desenvolvimento sustentável através da recuperação ambiental da Mata Atlântica, especialmente na bacia do Rio Doce, por meio de programas de educação ambiental e projetos de restauração ecológica.</p>
                 <div class="col-span-2 pb-8">
                     <a href="https://institutoterra.org/">
-                        <img src="https://assets.bileto.sympla.com.br/eventmanager/production/1l2v7e229u7bc02l3rl0fn5336uabh2teb4b4ar1deo3r1koibrh7qg9nk3qiirlom1k6qq3nb0gtk1j3oarra7e3ltipop0s1a3t8i.webp" alt=""> 
+                        <img src="InstitutoTerra.jpg" alt=""> 
                     </a>    
                 </div>
                 <p ><a class="text-4xl font-semibold tracking-tight text-green-900 hover:text-green-700 dark:text-white" href="https://www.gov.br/icmbio/pt-br">Instituto Chico Mendes de Conservação da Biodiversidade (ICMBio)</a></p>
                 <p class="mb-3 text-gray-500 dark:text-gray-400">O Instituto Chico Mendes de Conservação da Biodiversidade (ICMBio) é responsável por implementar a política nacional de unidades de conservação, gerenciando áreas federais como parques nacionais e reservas, e executando ações de proteção, monitoramento e fiscalização da biodiversidade. O órgão também atua na pesquisa, no desenvolvimento de planos de manejo, na recuperação de áreas degradadas e na gestão do turismo sustentável nessas áreas, como destaca Agência Gov e o eco.</p>
                 <div class="col-span-2 pb-8">
                     <a href="https://www.gov.br/icmbio/pt-br">
-                        <img src="https://ambito.com.br/wp-content/uploads/2020/08/manifestacao-instituto-chico-mendes.jpg" alt=""> 
+                        <img src="ICM.jpg" alt=""> 
                     </a>  
                 </div>
             </div>
@@ -47,14 +47,14 @@
                     <p class="mb-3 text-gray-500 dark:text-gray-400">A Fundação SOS Mata Atlântica é comprometida com a preservação da Mata Atlântica, desenvolvendo projetos de restauração florestal e conscientizando a população sobre a relevância desse bioma.  Eles também afetam as políticas públicas, garantindo a proteção de regiões essenciais. </p>
                     <div class="row-span-1 pb-8">
                         <a href="https://www.sosma.org.br/">
-                            <img src="https://sosma.org.br/storage/media/10ZAeZK2QSA1NKIP2r4yMyxI8awBXYqapQJ4elDv.jpg" alt=""> 
+                            <img src="/SOSMata.jpg" alt=""> 
                         </a>   
                     </div>
                     <p ><a class="text-4xl font-semibold tracking-tight text-green-900 hover:text-green-700 dark:text-white" href="https://ongzoe.org/">ONG ZOÉ</a></p>
                     <p class="mb-3 text-gray-500 dark:text-gray-400">A ONG Zoé é uma organização sem fins lucrativos que oferece apoio médico e psicológico a comunidades ribeirinhas e indígenas na Floresta Amazônica, com foco especial no estado do Pará.  Por meio de expedições que contam com médicos, enfermeiros e outros profissionais de saúde, a organização presta assistência a comunidades remotas, capacita agentes locais e investe em infraestrutura e equipamentos médico-hospitalares, visando estabelecer um suporte mais duradouro na área.</p>
                     <div class="col-span-2 pb-8">
                     <a href="https://ongzoe.org/">
-                        <img src="https://businessmoment.com.br/wp-content/uploads/2024/08/PHOTO-2024-08-14-10-35-47.webp" alt=""> 
+                        <img src="/mataatlatica.jpg" alt=""> 
                     </a>    
                     </div>
                    
@@ -62,7 +62,7 @@
                     <p class="mb-3 text-gray-500 dark:text-gray-400">A The Nature Conservancy (TNC) no Brasil é uma entidade ambiental dedicada à preservação da natureza e biodiversidade, com ênfase em soluções para a Amazônia, Cerrado e Mata Atlântica.  Ela trabalha no combate às mudanças climáticas, preservação de terras e águas, e promoção da restauração de ecossistemas por meio da ciência, colaborações com comunidades locais, governos e setor privado.</p>
                     <div class="col-span-2 pb-8">
                         <a href="https://www.tnc.org.br/">
-                            <img src="https://shopwhatsgood.com/cdn/shop/articles/WHATSGOOD_NON-PROFIT_blogsized_NATURE_CONSERVANCY_1198_x_750_px_1_1198x.jpg?v=1711383534" alt=""> 
+                            <img src="/NATURE_CONSERVANCY.jpg" alt=""> 
                         </a>  
                     </div>
                 </div>
