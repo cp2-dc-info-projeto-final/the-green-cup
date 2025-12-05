2d71df2e5894020b805cb51d600834679cbdcce8
labcaxias <labcaxias@LDC9C6B000EFEB8.labdc.zentyal>
2025-12-05 17:13:51 -0300

    Tamanho de imagens aumentada

--- src/api/app.js
+++ src/api/app.js
@@ -22,6 +22,7 @@ app.use(cors({
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.json({ limit: '1024mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
